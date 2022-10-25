rockandror/consuldemocracy

Add custom abilities for guest users

app/models/ability.rb
@@ -17,6 +17,8 @@ class Ability
         merge Abilities::Manager.new(user)
       elsif user.sdg_manager?
         merge Abilities::SDG::Manager.new(user)
+      elsif user.guest?
+        merge Abilities::Guest.new(user)
       else
         merge Abilities::Common.new(user)
       end

rockandror/innoviris-consul

app/models/custom/ability.rb
@@ -17,6 +17,8 @@ class Ability
         merge Abilities::Manager.new(user)
       elsif user.sdg_manager?
         merge Abilities::SDG::Manager.new(user)
+      elsif user.guest?
+        merge Abilities::Everyone.new(user)
       else
         merge Abilities::Common.new(user)
       end